97576131af72bcf1751f0c3af40b8101ac209c91
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-03 04:22:27 +0300

    dialogue component ui from shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
-
-export default function Home(){
-    return(
+import {UserButton } from '@clerk/nextjs'
+const SetupPage = () => {
+    return (
         <div className="p-4">
-            <Button>Button</Button>
+            <UserButton afterSignOutUrl='/'/>
         </div>
     )
-  }
+}
+
+
+export default SetupPage;
